47aada3845e637c2b05b7cba2609ab6d38d744d4
Cherif Jazra <>
2024-05-14 15:11:16 -0700

    add reverseIfTrue

---
+++ array/reverseIfTrue.py
@@ -0,0 +1,104 @@
+"""
+Reverse to Make Equal
+Given two arrays A and B of length N, determine if there is a way to make A equal to B by reversing any subarrays from array B any number of times.
+Signature
+
+bool areTheyEqual(int[] arr_a, int[] arr_b)
+
+Input
+All integers in array are in the range [0, 1,000,000,000].
+Output
+Return true if B can be made equal to A, return false otherwise.
+
+Example
+A = [1, 2, 3, 4]
+B = [1, 4, 3, 2]
+
+output = true
+After reversing the subarray of B from indices 1 to 3, array B will equal array A.
+
+"""
+
+# Add any extra import statements you may need here
+
+from typing import List
+from math import floor
+
+# Add any helper functions you may need here
+
+def reverse(myList: List[int], i: int, j: int):
+  # in place reversal
+  for idx in range(0,  floor((i + j) / 2)):
+    _temp = myList[j - idx]
+    myList[j - idx] = myList[i + idx]
+    myList[i + idx] = _temp
+  
+
+def _are_they_equal(array_a, array_b, startidx):
+  # Write your code here
+  
+  for idx in range(startidx, len(array_a)):
+    if array_a[idx] == array_b[idx]:
+       continue
+    
+    # find next value in array_b that matches a and reverse
+    equal = False
+    for idx2 in range(idx+1, len(array_a)):
+      if array_b[idx2] == array_a[idx]:
+        # reverse array b at this index and recurse through this function for any subsequent value matched
+        array_b_copy = array_b.copy()
+        reverse(array_b_copy, idx, idx2)  
+        equal = _are_they_equal(array_a, array_b_copy, idx + 1)        
+        if equal: # if any match, return 
+          return True
+    if not equal:
+      # not match
+      return False
+    
+  return True
+
+def are_they_equal(array_a, array_b):
+  return _are_they_equal(array_a, array_b, 0)
+
+
+# These are the tests we use to determine if the solution is correct.
+# You can add your own at the bottom.
+def printString(string):
+  print('[\"', string, '\"]', sep='', end='')
+
+test_case_number = 1
+
+def check(expected, output):
+  global test_case_number
+  result = False
+  if expected == output:
+    result = True
+  rightTick = '\u2713'
+  wrongTick = '\u2717'
+  if result:
+    print(rightTick, 'PASSED Test #', test_case_number, sep='')
+  else:
+    print(wrongTick, 'FAILED Test #', test_case_number, ': Expected ', sep='', end='')
+    printString(expected)
+    print(' Your output: ', end='')
+    printString(output)
+    print()
+  test_case_number += 1
+
+if __name__ == "__main__":
+  n_1 = 4
+  a_1 = [1, 2, 3, 4]
+  b_1 = [1, 4, 3, 2]
+  expected_1 = True
+  output_1 = are_they_equal(a_1, b_1)
+  check(expected_1, output_1)
+
+  n_2 = 4
+  a_2 = [1, 2, 3, 4]
+  b_2 = [1, 2, 3, 5]  
+  expected_2 = False
+  output_2 = are_they_equal(a_2, b_2)
+  check(expected_2, output_2)
+
+  # Add your own test cases here
+  
